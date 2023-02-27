5b6705308536ffa6997a03aeec7e1afee2e55071
Franco-ortellado <ortellado.frank@gmail.com>
2023-02-27 11:34:23 -0300

    updt_db

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Home from './components/Home/Home';
 import Detail from './components/Detail/Detail';
 import DogCreate from './components/DogCreate/DogCreate';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.baseURL = 'https://dogspi-production.up.railway.app/';
 
 function App() {
 	return (
